c495a72682efb112170269af5016eecdf9d4572d
Dorivika <dorivika@DESKTOP-VSML7F8.>
2025-01-18 16:08:28 -0500

    FIX : added appropriate types and costants for linux in tcpserver.h

--- include/TcpServer.h
+++ include/TcpServer.h
@@ -1,15 +1,24 @@
 #pragma once
 #include <string>
-#include <vector>
-#include <thread>
-#include <mutex>
 #include <functional>
+#include <thread>
 #include <unordered_map>
 
 #ifdef _WIN32
     #include <winsock2.h>
+    #include <ws2tcpip.h>
+    typedef SOCKET socket_t;
+    #define INVALID_SOCKET_VALUE INVALID_SOCKET
+    #define SOCKET_ERROR_VALUE SOCKET_ERROR
 #else
+    #include <arpa/inet.h>
+    #include <netinet/in.h>
     #include <sys/socket.h>
+    #include <unistd.h>
+    #include <errno.h>
+    typedef int socket_t;
+    #define INVALID_SOCKET_VALUE -1
+    #define SOCKET_ERROR_VALUE -1
 #endif
 
 namespace networking {
@@ -22,20 +31,19 @@ public:
     bool start();
     void stop();
     bool isRunning() const;
-    
+
     using MessageHandler = std::function<void(int clientId, const std::string& message)>;
     void setMessageHandler(MessageHandler handler);
 
 private:
     void acceptClients();
-    void handleClient(SOCKET clientSocket, int clientId);
-    
-    SOCKET serverSocket;
+    void handleClient(socket_t clientSocket, int clientId);
+
     int port;
     bool running;
+    socket_t serverSocket;
     std::thread acceptThread;
-    std::mutex clientsMutex;
-    std::unordered_map<SOCKET, std::thread> clientThreads;
+    std::unordered_map<socket_t, std::thread> clientThreads;
     MessageHandler messageHandler;
 };
 
